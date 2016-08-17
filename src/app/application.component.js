d897a5fbb57acee318f992d8a60425f2e301165b
= <=>
2016-08-17 14:37:40 -0300

    Seguindo convenções de Angular

---
+++ src/app/application.component.js
@@ -0,0 +1,17 @@
+(function() {
+	'use strict';
+	
+	angular
+		.module('application')
+		.component('application', {
+			templateUrl: '/app/application.template.html',
+			controller: MainCtrl
+		});
+
+	MainCtrl.$inject = ['$http'];
+
+	function MainCtrl($http) {
+	    var vm = this;
+	    vm.foo = 'bar';
+	  }
+})();
