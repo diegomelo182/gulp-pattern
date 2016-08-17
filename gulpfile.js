13642d97df7fe8b8209135fa4323d35a47cc17cf
Diego Melo <diego_melo@unit.br>
2016-08-17 14:57:39 -0300

    Tasks reorganizadas

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,11 @@ var gulp_uglify = require('gulp-uglify');
 var gulp_watch = require('gulp-watch');
 var sass = require('gulp-ruby-sass');
 var main_bower_files = require('main-bower-files');
+var runSequence = require('run-sequence');
 
 var files = {
 	src: {
+		module: 'src/app/**/*.js',
 		js: 'src/app/**/*.js',
 		sass: 'src/app/**/*.scss',
 		html: 'src/**/*.html',
@@ -24,73 +26,95 @@ var files = {
 	}
 }
 
-gulp.task('stream', function () {
-	return gulp_watch([
-		files.src.js,
-		files.src.sass,
-		files.src.html
-	],
-	function() {
-		var js_files_concat = main_bower_files();
-		js_files_concat = js_files_concat.concat([
-			files.src.js
-		]);
-
-		// javascripts
-		gulp.src(js_files_concat)
-			.pipe(gulp_concat(files.dist.js))
-			.pipe(gulp_rename('build.js'))
-			.pipe(gulp.dest(files.build.js));
-
-		// stylesheet
-		sass(files.src.sass, {
-            loadPath: [files.src.sass]
-        })
-        .pipe(gulp_concat(files.build.js))
-		.pipe(gulp_rename('build.css'))
-        .pipe(gulp.dest(files.build.css))
-
-		// index.html
-		gulp.src('src/index.html')
-			.pipe(gulp.dest(files.build.root));
-	},{ ignoreInitial: false });
+// dist
+gulp.task('js_dist', function(){
+	var js_files_concat = main_bower_files();
+	js_files_concat = js_files_concat.concat([
+		files.src.js
+	]);
+	console.log('Compilando JS --> dist ;)');
+	return gulp.src(js_files_concat)
+		.pipe(gulp_concat(files.dist.js))
+		.pipe(gulp_rename('main.js'))
+		.pipe(gulp_uglify())
+		.pipe(gulp.dest(files.dist.js));
 });
 
+gulp.task('sass_dist', function(){
+	console.log('Compilando Sass --> dist ;)');
+	return sass(files.src.sass, {
+		style: 'compressed',
+		loadPath: [files.src.sass]
+	})
+	.pipe(gulp_concat(files.dist.js))
+	.pipe(gulp_rename('main.css'))
+	.pipe(gulp.dest(files.dist.css));
+});
 
-gulp.task('callback', function () {
+gulp.task('html', function(){
+	// index.html
+	return gulp.src('src/index.html')
+		.pipe(gulp.dest(files.build.root));
+});
+
+// build
+gulp.task('js_build', function(){
+	var js_files_concat = main_bower_files();
+	js_files_concat = js_files_concat.concat([
+		files.src.js
+	]);
+
+	return gulp.src(js_files_concat)
+		.pipe(gulp_concat(files.dist.js))
+		.pipe(gulp_rename('main.js'))
+		.pipe(gulp.dest(files.build.js));
+});
+
+gulp.task('sass_build', function(){
+	return sass(files.src.sass, {
+		loadPath: [files.src.sass]
+	})
+	.pipe(gulp_concat(files.build.js))
+	.pipe(gulp_rename('main.css'))
+	.pipe(gulp.dest(files.build.css));
+});
+
+// watch build
+gulp.task('stream', function (callback) {
 	return gulp_watch([
 		files.src.js,
 		files.src.sass,
 		files.src.html
 	],
-	function () {
-		var js_files_concat = main_bower_files();
-		js_files_concat = js_files_concat.concat([
-			files.src.js
-		]);
-		// javascripts
-		console.log('Compilando JS ;)');
-		gulp.src(js_files_concat)
-			.pipe(gulp_concat(files.dist.js))
-			.pipe(gulp_rename('dist.min.js'))
-			.pipe(gulp_uglify())
-			.pipe(gulp.dest(files.dist.js));
-
-		// stylesheet
-		console.log('Compilando Sass ;)');
-		sass(files.src.sass, {
-            style: 'compressed',
-            loadPath: [files.src.sass]
-        })
-        .pipe(gulp_concat(files.dist.js))
-		.pipe(gulp_rename('dist.min.css'))
-        .pipe(gulp.dest(files.dist.css));
-
-        // index.html
-        console.log('Copiando o index.html ;)');
-		gulp.src('src/index.html')
-			.pipe(gulp.dest(files.dist.root));
+	function(){
+		runSequence(
+			'js_dist',
+			'sass_dist',
+			'js_build',
+			'sass_build',
+			'html',
+			callback
+		);
 	});
 });
 
-gulp.task('default', ['stream', 'callback']);
+// watch dist
+gulp.task('callback', function (callback) {
+	return gulp_watch([
+		files.src.js,
+		files.src.sass,
+		files.src.html
+	],
+	function(){
+		runSequence(
+			'js_dist',
+			'sass_dist',
+			'js_build',
+			'sass_build',
+			'html',
+			callback
+		);
+	});
+});
+
+gulp.task('default', ['js_dist', 'sass_dist','js_build', 'sass_build', 'html', 'stream', 'callback']);
