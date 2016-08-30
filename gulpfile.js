98e25e6bc0eaa06e5ab685f9cd51fa8fc7b6e565
Diego Melo <diego_melo@unit.br>
2016-08-30 12:06:10 -0300

    Inserida a funcionalidade de deletar antes do build e json

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,7 @@ var connect = require('gulp-connect');
 var jade = require('gulp-jade');
 var cache = require('gulp-cache');
 var image = require('gulp-image');
+var del = require('del');
 
 var files = {
 	src: {
@@ -19,21 +20,24 @@ var files = {
 		jade: 'src/**/*.jade',
 		fonts: 'src/fonts/**/*.{ttf,eot,woff,woff2,svg}',
 		fonts_bower: 'bower_components/**/*.{ttf,eot,woff,woff2,svg}',
-		imgs: 'src/imgs/**/*.{jpg,jpeg,gif,png}'
+		imgs: 'src/imgs/**/*.{jpg,jpeg,gif,png}',
+		json: 'src/json/**/*.json'
 	},
 	dist: {
 		root: 'dist',
 		js: 'dist/app',
 		css: 'dist/css',
 		fonts: 'dist/fonts',
-		imgs: 'dist/imgs'
+		imgs: 'dist/imgs',
+		json: 'dist/json'
 	},
 	build: {
 		root: 'build',
 		js: 'build/app',
 		css: 'build/css',
 		fonts: 'build/fonts',
-		imgs: 'build/imgs'
+		imgs: 'build/imgs',
+		json: 'build/json'
 	}
 }
 
@@ -49,13 +53,19 @@ gulp.task('js_dist', function() {
 		files.src.module,
 		files.src.js
 	]);
-	console.log('Compilando JS --> dist ;)');
 	return gulp.src(js_files_concat)
 		.pipe(gulp_concat(files.dist.js))
 		.pipe(gulp_rename('main.js'))
 		.pipe(gulp_uglify())
 		.on('error', on_error)
 		.pipe(gulp.dest(files.dist.js));
+});
+
+// js dist remove
+gulp.task('rm_js_dist', function() {
+	return del(['dist/*.js']).then(paths => {
+		console.log('Arquivos e diretorios removidos:\n', paths.join('\n'));
+	});
 });
 
 // sass dist
@@ -71,6 +81,13 @@ gulp.task('sass_dist', function() {
 	.pipe(gulp.dest(files.dist.css));
 });
 
+// css dist remove
+gulp.task('rm_sass_dist', function() {
+	return del(['dist/*.css']).then(paths => {
+		console.log('Arquivos e diretorios removidos:\n', paths.join('\n'));
+	});
+});
+
 // html dist
 gulp.task('html_dist', function() {
 	return gulp.src('src/index.jade')
@@ -79,6 +96,13 @@ gulp.task('html_dist', function() {
 	}))
 	.on('error', on_error)
 	.pipe(gulp.dest(files.dist.root));
+});
+
+// html dist remove
+gulp.task('rm_html_dist', function() {
+	return del(['dist/index.html']).then(paths => {
+		console.log('Arquivos e diretorios removidos:\n', paths.join('\n'));
+	});
 });
 
 // html build
@@ -92,6 +116,13 @@ gulp.task('html_build', function() {
 	.pipe(gulp.dest(files.build.root));
 });
 
+// html build remove
+gulp.task('rm_html_build', function() {
+	return del(['build/index.html']).then(paths => {
+		console.log('Arquivos e diretorios removidos:\n', paths.join('\n'));
+	});
+});
+
 // html components dist
 gulp.task('html_components_dist', function() {
 	return gulp.src('src/app/**/*.jade')
@@ -101,6 +132,13 @@ gulp.task('html_components_dist', function() {
 		.on('error', on_error)
 		.pipe(gulp.dest(files.dist.js))
 		.pipe(connect.reload());
+});
+
+// html components dist remove
+gulp.task('rm_html_components_dist', function() {
+	return del(['dist/app/**/*.html']).then(paths => {
+		console.log('Arquivos e diretorios removidos:\n', paths.join('\n'));
+	});
 });
 
 // html components build
@@ -113,6 +151,13 @@ gulp.task('html_components_build', function() {
 		.on('error', on_error)
 		.pipe(gulp.dest(files.build.js))
 		.pipe(connect.reload());
+});
+
+// html components build remove
+gulp.task('rm_html_components_build', function() {
+	return del(['build/app/**/*.html']).then(paths => {
+		console.log('Arquivos e diretorios removidos:\n', paths.join('\n'));
+	});
 });
 
 // js build
@@ -130,6 +175,13 @@ gulp.task('js_build', function() {
 		.pipe(gulp.dest(files.build.js));
 });
 
+// js build remove
+gulp.task('rm_js_build', function() {
+	return del(['build/*.js']).then(paths => {
+		console.log('Arquivos e diretorios removidos:\n', paths.join('\n'));
+	});
+});
+
 // sass build
 gulp.task('sass_build', function() {
 	return sass(files.src.sass, {
@@ -141,6 +193,13 @@ gulp.task('sass_build', function() {
 	.pipe(gulp.dest(files.build.css));
 });
 
+// css build remove
+gulp.task('rm_sass_build', function() {
+	return del(['build/*.css']).then(paths => {
+		console.log('Arquivos e diretorios removidos:\n', paths.join('\n'));
+	});
+});
+
 // image build
 gulp.task('image_build', function () {
 	return gulp.src(files.src.imgs)
@@ -149,12 +208,26 @@ gulp.task('image_build', function () {
 		.pipe(gulp.dest(files.build.imgs));
 });
 
+// image build remove
+gulp.task('rm_image_build', function() {
+	return del(['build/imgs/*.{jpg,jpeg,gif,png}']).then(paths => {
+		console.log('Arquivos e diretorios removidos:\n', paths.join('\n'));
+	});
+});
+
 // image dist
 gulp.task('image_dist', function () {
 	return gulp.src(files.src.imgs)
 		.pipe(image())
 		.on('error', on_error)
 		.pipe(gulp.dest(files.dist.imgs));
+});
+
+// image dist remove
+gulp.task('rm_image_dist', function() {
+	return del(['dist/imgs/*.{jpg,jpeg,gif,png}']).then(paths => {
+		console.log('Arquivos e diretorios removidos:\n', paths.join('\n'));
+	});
 });
 
 // fonts dist
@@ -168,6 +241,13 @@ gulp.task('fonts_dist', function () {
 		.pipe(gulp.dest(files.dist.fonts));	
 });
 
+// fonts dist remove
+gulp.task('rm_fonts_dist', function() {
+	return del(['dist/fonts/*.{ttf,eot,woff,woff2,svg}']).then(paths => {
+		console.log('Arquivos e diretorios removidos:\n', paths.join('\n'));
+	});
+});
+
 // fonts build
 gulp.task('fonts_build', function () {
 	gulp.src(files.src.fonts)
@@ -177,6 +257,41 @@ gulp.task('fonts_build', function () {
 	gulp.src(files.src.fonts_bower)
 		.on('error', on_error)
 		.pipe(gulp.dest(files.build.fonts));	
+});
+
+// fonts build remove
+gulp.task('rm_fonts_build', function() {
+	return del(['build/fonts/*.{ttf,eot,woff,woff2,svg}']).then(paths => {
+		console.log('Arquivos e diretorios removidos:\n', paths.join('\n'));
+	});
+});
+
+// json build
+gulp.task('json_build', function () {
+	return gulp.src(files.src.json)
+		.on('error', on_error)
+		.pipe(gulp.dest(files.build.json));
+});
+
+// json build remove
+gulp.task('rm_json_build', function() {
+	return del(['build/json/*.json']).then(paths => {
+		console.log('Arquivos e diretorios removidos:\n', paths.join('\n'));
+	});
+});
+
+// json dist
+gulp.task('json_dist', function () {
+	return gulp.src(files.src.json)
+		.on('error', on_error)
+		.pipe(gulp.dest(files.dist.json));
+});
+
+// json dist remove
+gulp.task('rm_json_dist', function() {
+	return del(['dist/json/*.json']).then(paths => {
+		console.log('Arquivos e diretorios removidos:\n', paths.join('\n'));
+	});
 });
 
 // connect build
@@ -218,6 +333,7 @@ gulp.task('stream', function(callback) {
 	files.src.js,
 	function() {
 		runSequence(
+			'rm_js_build',
 			'js_build',
 			'clear_cache',
 			'reload'
@@ -229,6 +345,7 @@ gulp.task('stream', function(callback) {
 	files.src.sass,
 	function() {
 		runSequence(
+			'rm_sass_build',
 			'sass_build',
 			'clear_cache',
 			'reload'
@@ -240,7 +357,9 @@ gulp.task('stream', function(callback) {
 	files.src.jade,
 	function() {
 		runSequence(
+			'rm_html_build',
 			'html_build',
+			'rm_html_components_build',
 			'html_components_build',
 			'clear_cache',
 			'reload'
@@ -252,6 +371,7 @@ gulp.task('stream', function(callback) {
 	files.src.imgs,
 	function() {
 		runSequence(
+			'rm_image_build',
 			'image_build',
 			'clear_cache',
 			'reload'
@@ -263,6 +383,7 @@ gulp.task('stream', function(callback) {
 	files.src.fonts,
 	function() {
 		runSequence(
+			'rm_fonts_build',
 			'fonts_build',
 			'clear_cache',
 			'reload'
@@ -274,7 +395,20 @@ gulp.task('stream', function(callback) {
 	files.src.fonts_bower,
 	function() {
 		runSequence(
+			'rm_fonts_build',
 			'fonts_build',
+			'clear_cache',
+			'reload'
+		);
+	});
+
+	// json bower
+	gulp_watch(
+	files.src.json,
+	function() {
+		runSequence(
+			'rm_json_build',
+			'json_build',
 			'clear_cache',
 			'reload'
 		);
@@ -288,6 +422,7 @@ gulp.task('callback', function(callback) {
 	files.src.js,
 	function() {
 		runSequence(
+			'rm_js_dist',
 			'js_dist',
 			'clear_cache',
 			'reload'
@@ -299,6 +434,7 @@ gulp.task('callback', function(callback) {
 	files.src.sass,
 	function() {
 		runSequence(
+			'rm_sass_dist',
 			'sass_dist',
 			'clear_cache',
 			'reload'
@@ -310,7 +446,9 @@ gulp.task('callback', function(callback) {
 	files.src.jade,
 	function() {
 		runSequence(
+			'rm_html_dist',
 			'html_dist',
+			'rm_html_components_dist',
 			'html_components_dist',
 			'clear_cache',
 			'reload'
@@ -322,6 +460,7 @@ gulp.task('callback', function(callback) {
 	files.src.imgs,
 	function() {
 		runSequence(
+			'rm_image_dist',
 			'image_dist',
 			'clear_cache',
 			'reload'
@@ -333,6 +472,7 @@ gulp.task('callback', function(callback) {
 	files.src.fonts,
 	function() {
 		runSequence(
+			'rm_fonts_dist',
 			'fonts_dist',
 			'clear_cache',
 			'reload'
@@ -344,11 +484,25 @@ gulp.task('callback', function(callback) {
 	files.src.fonts_bower,
 	function() {
 		runSequence(
+			'rm_fonts_dist',
 			'fonts_dist',
 			'clear_cache',
 			'reload'
 		);
 	});
+
+	// json bower
+	gulp_watch(
+	files.src.json,
+	function() {
+		runSequence(
+			'rm_json_dist',
+			'json_dist',
+			'clear_cache',
+			'reload'
+		);
+	});
+
 });
 
 gulp.task(
@@ -361,6 +515,7 @@ function(callback){
 		'html_components_build',
 		'image_build',
 		'fonts_build',
+		'json_build',
 		'connect_build',
 		'stream'
 	);
@@ -376,6 +531,7 @@ function(callback){
 		'html_components_dist',
 		'image_dist',
 		'fonts_dist',
+		'json_dist',
 		'connect_dist',
 		'callback'
 	);
